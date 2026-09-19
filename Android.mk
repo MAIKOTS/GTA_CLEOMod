a3faa46f303bf9dba1e52861f939ac9e2041b78f
Maiko.T.S <maiko.t.s.ofc@gmail.com>
2026-09-19 18:49:29 -0300

    Atualizar o Android.mk

--- Android.mk
+++ Android.mk
@@ -1,20 +1,15 @@
 LOCAL_PATH := $(call my-dir)
 
-# Importa estaticamente o módulo de filesystem do próprio NDK r21
-include $(CLEAR_VARS)
-LOCAL_MODULE := c++fs_static
-LOCAL_SRC_FILES := $(NDK_ROOT)/sources/cxx-stl/llvm-libc++/libs/$(TARGET_ARCH_ABI)/libc++fs.a
-include $(PREBUILT_STATIC_LIBRARY)
-
-# Compilador do seu Mod
 include $(CLEAR_VARS)
 LOCAL_CPP_EXTENSION := .cpp .cc
 LOCAL_MODULE    := CLEOMod
 LOCAL_SRC_FILES := main.cpp mod/logger.cpp mod/config.cpp libcleo.cpp cleo201_refactor.cpp cleo4opcodes.cpp cleo5opcodes.cpp cleomath.cpp mini-scanf-cleo/c_scan.c
+
 LOCAL_CFLAGS += -O2 -mfloat-abi=softfp -DNDEBUG -std=c11 -DAML_CLEO
 LOCAL_CXXFLAGS += -O2 -mfloat-abi=softfp -DNDEBUG -std=c++17
 
-LOCAL_STATIC_LIBRARIES := c++fs_static
 LOCAL_LDLIBS := -llog
+# No NDK r21, o Clang reconhece a biblioteca de filesystem quando passada via -L com a pasta interna de toolchains
+LOCAL_LDFLAGS += -L$(NDK_ROOT)/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib/arm-linux-androideabi -lc++fs
 
 include $(BUILD_SHARED_LIBRARY)
