dcbf253bfbc239ab140744fdb0bf45cf96cd2b85
Maiko.T.S <maiko.t.s.ofc@gmail.com>
2026-09-19 14:09:39 -0300

    Update LOCAL_LDLIBS to include c++fs
    Added -lc++fs to LOCAL_LDLIBS for NDK r21 compatibility.

--- Android.mk
+++ Android.mk
@@ -6,6 +6,8 @@ LOCAL_MODULE    := CLEOMod
 LOCAL_SRC_FILES := main.cpp mod/logger.cpp mod/config.cpp libcleo.cpp cleo201_refactor.cpp cleo4opcodes.cpp cleo5opcodes.cpp cleomath.cpp mini-scanf-cleo/c_scan.c
 LOCAL_CFLAGS += -O2 -mfloat-abi=softfp -DNDEBUG -std=c11 -DAML_CLEO
 LOCAL_CXXFLAGS += -O2 -mfloat-abi=softfp -DNDEBUG -std=c++17
-LOCAL_LDLIBS := -llog
+
+# O -lc++fs e essencial no NDK r21 para linkar o <filesystem>
+LOCAL_LDLIBS := -llog -lc++fs
 
 include $(BUILD_SHARED_LIBRARY)
