ed9695a60c7a520ec7985fcb4c31a19af80f040e
Maiko.T.S <maiko.t.s.ofc@gmail.com>
2026-09-19 13:51:16 -0300

    Add additional libraries to LOCAL_LDLIBS

--- Android.mk
+++ Android.mk
@@ -6,5 +6,5 @@ LOCAL_MODULE    := CLEOMod
 LOCAL_SRC_FILES := main.cpp mod/logger.cpp mod/config.cpp libcleo.cpp cleo201_refactor.cpp cleo4opcodes.cpp cleo5opcodes.cpp cleomath.cpp mini-scanf-cleo/c_scan.c
 LOCAL_CFLAGS += -O2 -mfloat-abi=softfp -DNDEBUG -std=c11 -DAML_CLEO
 LOCAL_CXXFLAGS += -O2 -mfloat-abi=softfp -DNDEBUG -std=c++17
-LOCAL_LDLIBS += -llog
+LOCAL_LDLIBS := -llog -landroid -lc++fs
 include $(BUILD_SHARED_LIBRARY)
