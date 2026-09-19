dc64753ab3bf736dc8167c54781d2b6bd190e6da
Maiko.T.S <maiko.t.s.ofc@gmail.com>
2026-09-19 14:06:23 -0300

    Remove unnecessary C++ standard library flags

--- Android.mk
+++ Android.mk
@@ -6,7 +6,6 @@ LOCAL_MODULE    := CLEOMod
 LOCAL_SRC_FILES := main.cpp mod/logger.cpp mod/config.cpp libcleo.cpp cleo201_refactor.cpp cleo4opcodes.cpp cleo5opcodes.cpp cleomath.cpp mini-scanf-cleo/c_scan.c
 LOCAL_CFLAGS += -O2 -mfloat-abi=softfp -DNDEBUG -std=c11 -DAML_CLEO
 LOCAL_CXXFLAGS += -O2 -mfloat-abi=softfp -DNDEBUG -std=c++17
-LOCAL_LDLIBS := -llog -lc++fs
-LOCAL_LDFLAGS += -lc++fs
+LOCAL_LDLIBS := -llog
 
 include $(BUILD_SHARED_LIBRARY)
