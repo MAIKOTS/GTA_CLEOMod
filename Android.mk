8eb9006a554364eb9d65dfc5c9ca4ef43763de8a
Maiko.T.S <maiko.t.s.ofc@gmail.com>
2026-09-19 18:38:25 -0300

    Atualizar o Android.mk

--- Android.mk
+++ Android.mk
@@ -7,4 +7,7 @@ LOCAL_SRC_FILES := main.cpp mod/logger.cpp mod/config.cpp libcleo.cpp cleo201_re
 LOCAL_CFLAGS += -O2 -mfloat-abi=softfp -DNDEBUG -std=c11 -DAML_CLEO
 LOCAL_CXXFLAGS += -O2 -mfloat-abi=softfp -DNDEBUG -std=c++17
 LOCAL_LDLIBS += -llog
+# Força o linker do NDK r21 a incluir todos os símbolos da biblioteca de filesystem
+LOCAL_LDFLAGS += -Wl,--whole-archive -lc++_experimental -Wl,--no-whole-archive
+
 include $(BUILD_SHARED_LIBRARY)
